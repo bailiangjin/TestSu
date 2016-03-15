964a185d06772c2ecd5e739e5429ad6473401ff7
Kevin Bai <bailiangjin@qq.com>
2016-03-15 10:43:54 +0800

    添加 Application类

---
+++ app/src/main/java/com/example/testsu/MyApplication.java
@@ -0,0 +1,9 @@
+package com.example.testsu;
+
+import com.kevin.baselibrary.app.SuperApplication;
+
+/**
+ * Created by xywy on 2016/3/15.
+ */
+public class MyApplication extends SuperApplication {
+}
